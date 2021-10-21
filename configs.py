7e1b93b6378591cdc519fbeffd8ee1c462d557ee
Liku cs <83144124+likucs@users.noreply.github.com>
2021-10-21 16:21:41 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,8 @@ class Config(object):
     SESSION_NAME = os.environ.get("SESSION_NAME", "Rename-Bot-0")
     SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 5))
     BOT_OWNER = os.environ.get("BOT_OWNER", 1445283714)
-    CAPTION = "Rename Bot by @{}\n\nMade by @AbirHasan2005"
+    CAPTION = ""━━━━━━━━━ ✧ ━━━━━━━━\n&ensp;💢<a href='https://t.me/iAmLiKu1'>@iAmLiKu1</a" \
+                         ">💢\n━━━━━━━━━ ✧ ━━━━━━━━\n\n🎗<b>ʝσιи🎗ѕнαяє🎗ѕυρρσят</b>🎗 "
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
